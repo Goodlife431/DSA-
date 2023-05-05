e1d8d63ed8ad599a3e4c78659da4410379dd6e34
Goodlife <oduwolejohn431@gmail.com>
2023-05-05 09:54:13 +0100

    java

---
+++ ProblemSolvingLevel1.java
@@ -0,0 +1,116 @@
+package Problem_solving;
+import java.util.Scanner;
+
+
+public class ProblemSolvingLevel1 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    // number 1
+    public static int secondLargest(int... numbers){
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+        for (int number : numbers) {
+            if (number > highest) {
+                secondHighest = highest;
+                highest = number;
+            }
+        }
+        return secondHighest;
+    }
+
+    // number 2
+    public static void printSemicolon() {
+        String semicolon = "SEMICOLON";
+        for (int i = 0; i < semicolon.length(); i++) {
+            System.out.println(semicolon.charAt(i));
+        }
+    }
+
+    // number 3
+    public static void printVentures() {
+        String ventures = "VENTURES";
+        for (int i = 0; i < ventures.length(); i++) {
+            System.out.println(ventures.charAt(i) + " " + ventures.charAt(++i));
+        }
+    }
+
+    // number 4
+    public static void findOccurrences() {
+        String word = "Mississippi";
+        int countForS = 0;
+        int countForI = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 's') {
+                countForS++;
+            } else if (word.charAt(i) == 'i') {
+                countForI++;
+            }
+        }
+        System.out.println("Number of 's': " + countForS);
+        System.out.println("Number of 'i': " + countForI);
+
+    }
+
+    // number 5
+    public static int sumDivisibleBy3() {
+        int sum = 0;
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0) sum += i;
+        }
+        return sum;
+    }
+
+    // number 6
+    public static void guessBirthDate() {
+        int birthday = 15;
+        int answer = 0;
+        while (answer != birthday) {
+            System.out.println("Enter a guess: ");
+            answer = scanner.nextInt();
+            if (answer != birthday) System.out.println("Incorrect Guess!");
+        }
+        System.out.println("Correct Guess!");
+    }
+
+    // number 7
+    public static void infiniteSeries() {
+        double pie = 4.0;
+        double denominator = 3.0;
+        boolean found = false;
+        int terms = 1;
+
+        while (!found) {
+            if (terms % 2 == 0) {
+                pie += 4.0 / denominator;
+            } else {
+                pie -= 4.0 / denominator;
+            }
+            denominator += 2.0;
+            System.out.printf("%-10d%.7f%n", terms, pie);
+            terms++;
+
+            if (String.format("%.7f", pie).equals("3.14159")) {
+                found = true;
+            }
+        }
+
+        System.out.println("Pie: " + pie);
+        System.out.println("Number of terms: " + terms);
+    }
+    
+    public static void main(String[] args) {
+        printHelloWorld();
+        printSemicolon();
+        printVentures();
+        System.out.println(secondLargest(2, 4, 70, 12, 55, 90, 532, 45, 12)); // prints 90
+        findOccurrences(); // 4 occurrences each
+        System.out.println(sumDivisibleBy3()); // prints 165
+        guessBirthDate();
+        infiniteSeries();
+    }
+
+}
---
+++ ProblemSolvingLevel2.java
@@ -0,0 +1,113 @@
+package Problem_solving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemSolvingLevel2 {
+    public static int[] mergeTwoSortedArrays(int[] array1, int[] array2) {
+        int[] sortedBothArrays = new int[array1.length + array2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                sortedBothArrays[k++] = array1[i++];
+            } else {
+                sortedBothArrays[k++] = array2[j++];
+            }
+        }
+        while (i < array1.length) {
+            sortedBothArrays[k++] = array1[i++];
+        }
+        while (j < array2.length) {
+            sortedBothArrays[k++] = array2[j++];
+        }
+        return sortedBothArrays;
+    }
+
+    public static int convertToInt(int newNumber) {
+        int sum = 0;
+        int counter = 0;
+        for (int length = String.valueOf(newNumber).length(); length != 0; newNumber /= 10) {
+            int r = newNumber % 10;
+            int decimal = r * (int) Math.pow(2,  counter);
+            sum += decimal;
+            counter++;
+            length--;
+        }
+        return sum;
+    }
+
+    public static double[] productOfAllElements(double[] initialList) {
+        double[] result = new double[initialList.length];
+        for (int i = 0; i < result.length; i++) {
+            double multiply = 1;
+            for (int j = 0; j < initialList.length; j++) {
+                if (i == j) continue;
+                multiply *= initialList[j];
+            }
+            result[i] = multiply;
+        }
+        return result;
+    }
+
+    public static int nonRepeatingNumber(int[] numbers) {
+        int result = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int digit = numbers[i];
+            boolean exists = false;
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (digit == numbers[j]) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                result = digit;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static int[] rightRotate(int[] digits) {
+        int[] result = new int[digits.length];
+        result[0] = digits[digits.length - 1];
+        for (int i = 1; i < result.length; i++) {
+            result[i] = digits[i - 1];
+        }
+        return result;
+    }
+
+    public static int[] rearrangePositiveNegative(int[] digits) {
+        for (int i = 0; i < digits.length; i++) {
+            for (int j = 0; j < digits.length; j++) {
+                if (digits[i] < digits[j]) {
+                    int temp = digits[i];
+                    digits[i] = digits[j];
+                    digits[j] = temp;
+                }
+            }
+        }
+        return digits;
+    }
+
+    public static double maximumSublistSum(int[] numbers) {
+        double sumOfNumbers = 0;
+        List<Double> sums = new ArrayList<>();
+        for (int number : numbers) {
+            if (number < 0) {
+                sums.add(sumOfNumbers);
+                sumOfNumbers  = 0;
+            }
+            else {
+                sumOfNumbers += number;
+            }
+        }
+        double highest = 0;
+        for (double number : sums) {
+            if (number > highest) highest = number;
+        }
+        return highest;
+    }
+
+}
+
